6204acaaa337925da95686c39f101c1fe3055969
Francososa <franky.sosa@gmail.com>
2018-09-16 23:36:13 -0600

    Generalize list functions to work with all monads

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,6 +4,7 @@ module Lib
 
 import Lib.Types
 import Control.Monad
+import Control.Applicative
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
@@ -27,20 +28,20 @@ courses = [Course 101 "French" 100
 
           
 
-_select :: (a -> b) -> [a] -> [b]
+_select :: Monad m => (a -> b) -> m a -> m b
 _select prop vals = do
   val <- vals
   return (prop val)
 
 
-_where :: (a -> Bool) -> [a] -> [a]
+_where :: (Monad m, Alternative m) => (a -> Bool) -> m a -> m a
 _where test vals = do
   val <- vals
   guard (test val)
   return val
 
 
-_join :: Eq c => [a] -> [b] -> (a -> c) -> (b -> c) -> [(a,b)]
+_join :: (Monad m, Alternative m, Eq c) => m a -> m a -> (a -> c) -> (b -> c) -> m (a,b)
 _join data1 data2 prop1 prop2 = do
   d1 <- data1
   d2 <- data2
